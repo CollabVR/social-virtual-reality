df395330cb9d278f4ec60cf3d45eadcd1d20f411
bygerman <by.german@outlook.com>
2023-10-15 05:25:07 -0500

    feat: add mouth movement when speaking

--- Assets/Scripts/PlayerInformationCanvas.cs
+++ Assets/Scripts/PlayerInformationCanvas.cs
@@ -9,7 +9,8 @@ using UnityEngine.UI;
 public class PlayerInformationCanvas : MonoBehaviour
 {
     public TMP_Text nickname;
-    // public Image speakerImage;
+    public Transform mouth;
+
     public Transform speakerImage;
 
     public Transform canvasPlayerInformation;
@@ -31,7 +32,7 @@ public class PlayerInformationCanvas : MonoBehaviour
 
         if (photonView.Owner == null) return;
         if (nickname.text == photonView.Owner.NickName) return;
-        
+
         nickname.text = photonView.Owner.NickName;
     }
 
@@ -39,8 +40,27 @@ public class PlayerInformationCanvas : MonoBehaviour
     {
         Debug.Log("SetSpeakerImage");
         if (!photonView.IsMine) return;
-        
+
         speakerImage.transform.localScale = new Vector3(1, photonVoiceView.IsRecording ? 1 : 0, 1);
+
+
+        if (photonVoiceView.IsRecording)
+        {
+            mouth.transform.localScale = new Vector3(x: 0.06f, y: 0.03f, z: 0.006f);
+        }
+        else
+        {
+            mouth.transform.localScale = new Vector3(x: 0.06f, y: 0f, z: 0f);
+        }
+
+        // var MicrophoneDevice = photonVoiceView.RecorderInUse.MicrophoneDevice;
+        // Debug.Log("Microphone device: " + MicrophoneDevice.Name);
+
+        // // microphone devices
+        // foreach (var device in Microphone.devices)
+        // {
+        //     Debug.Log("Name: " + device);
+        // }
     }
 
 }
